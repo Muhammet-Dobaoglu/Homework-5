34e349f2f824983d3b1667151a621417eb3ccbd1
Muhammet Dobaoğlu <muhammetdobaoglu@hotmail.com>
2022-10-10 19:04:00 +0300

    Proje dosyası ekle.

---
+++ Homework-5/Program.cs
@@ -0,0 +1,29 @@
+﻿#region Üniversite Not Hesapla
+
+using System.Globalization;
+
+Console.Write("Vize Notunuz : ");
+int vize = int.Parse(Console.ReadLine());
+
+Console.Write("Final Notunuz : ");
+int final = int.Parse(Console.ReadLine());
+
+double ort = vize * .4 + final * .6;
+
+
+if (ort>=50)
+{
+    Console.WriteLine($"Notunuz : {ort} ");
+}
+
+else
+{
+    Console.Write("Bütünleme Notunuz : ");
+    int but = int.Parse(Console.ReadLine());
+    double ort2 = vize * .4 + but * .6;
+
+    Console.WriteLine($"Notunuz : {ort2}");
+}
+
+
+#endregion
